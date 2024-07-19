a9c20d9a1525037acdcd644b45dca2a3af51ca1b
minhhieudev <minhhieudev31@gmail.com>
2024-07-19 22:43:35 +0700

    add update profile user

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,20 +1,21 @@
 import { Inter } from "next/font/google";
-
 import "../globals.css";
 import Provider from "@components/Provider";
+import TopBar from "@components/TopBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Halo Chat App",
-  description: "Build a Next 14 Chat App",
+  description: "A Next.js 14 Chat App ",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-purple-1`}>
+      <body className={`${inter.className} bg-blue-2`}>
         <Provider>
+          <TopBar />
           {children}
         </Provider>
       </body>
